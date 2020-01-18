0842aa85ac9c745700fda7a1e5452128c2ac2486
Miro Mannino <miro.mannino@gmail.com>
2020-01-18 13:22:10 +0400

    style changes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,15 @@ module.exports = {
   'rules': {
     'no-empty-source': null,
     'string-quotes': 'double',
+    'indentation': 4,
+    'declaration-colon-newline-after': null,
+    'value-list-comma-newline-after': null,
+    'rule-empty-line-before': null,
+    'comment-empty-line-before': null,
+    'block-closing-brace-empty-line-before': null,
+    'block-opening-brace-newline-after': null,
+    'no-descending-specificity': null,
+    'no-missing-end-of-source-newline': null,
     'at-rule-no-unknown': [
       true,
       {
