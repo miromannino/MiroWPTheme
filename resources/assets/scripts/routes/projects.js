15b8ee1ec1b2603507dd855730f15b3b1260d04a
Miro Mannino <miro.mannino@gmail.com>
2020-05-14 01:00:27 +0400

    portfolio changes

--- resources/assets/scripts/routes/projects.js
+++ resources/assets/scripts/routes/projects.js
@@ -1,56 +1,60 @@
 import $ from 'jquery';
+import Bricklayer from 'bricklayer';
 
 export default {
   init() {
-    // JavaScript to be fired on the projects
+
   },
   finalize() {
-    // JavaScript to be fired on the home page, after the init JS
+    $(document).ready(function () {
+      var bricklayer = new Bricklayer(document.querySelector('.bricklayer'));
 
-    $('.portfolio').addClass('visible');
-
-    var selectedTag = null;
-    function updateTagsAndCards() {
-      $('.portfolio-filter .tag').each(function (idx, el) {
-        var $el = $(el);
-        if ($el.text().toLowerCase() === selectedTag) {
-          $el.removeClass('tag-disabled');
-        } else {
-          $el.addClass('tag-disabled');
-        }
-      });
-      if (selectedTag === null) {
-        $('.portfolio-filter .tag-all').removeClass('tag-disabled');
-      }
-      $('.portfolio .card').each(function (idx, el) {
-        var $entry = $(el);
+      $('.portfolio').addClass('visible');
+  
+      var selectedTag = null;
+      function updateTagsAndCards() {
+        $('.portfolio-filter .tag').each(function (idx, el) {
+          var $el = $(el);
+          if ($el.text().toLowerCase() === selectedTag) {
+            $el.removeClass('tag-disabled');
+          } else {
+            $el.addClass('tag-disabled');
+          }
+        });
         if (selectedTag === null) {
-          $entry.removeClass('card-hide');
-        } else {
-          var tagFound = false;
-          $entry.find('.project-tags>li').each(function (idx, el) {
-            if ($(el).text().toLowerCase() === selectedTag) tagFound = true;
-          });
-          if (tagFound || $entry.hasClass('card-always-visible')) {
+          $('.portfolio-filter .tag-all').removeClass('tag-disabled');
+        }
+        $('.portfolio .card').each(function (idx, el) {
+          var $entry = $(el);
+          if (selectedTag === null) {
             $entry.removeClass('card-hide');
           } else {
-            $entry.addClass('card-hide');
+            var tagFound = false;
+            $entry.find('.project-tags>li').each(function (idx, el) {
+              if ($(el).text().toLowerCase() === selectedTag) tagFound = true;
+            });
+            if (tagFound || $entry.hasClass('card-always-visible')) {
+              $entry.removeClass('card-hide');
+            } else {
+              $entry.addClass('card-hide');
+            }
           }
-        }
+        });
+        bricklayer.redraw();
+        console.log('selected: ', selectedTag);
+      }
+  
+      $('.portfolio-filter .tag-all').click(function () {
+        selectedTag = null;
+        updateTagsAndCards();
       });
-      console.log('selected: ', selectedTag);
-    }
+  
+      $('.portfolio-filter a:not(.tag-all)').click(function (ev) {
+        var tag = $(ev.target).text().toLowerCase();
+        selectedTag = selectedTag === tag ? null : tag;
+        updateTagsAndCards();
+      });
 
-    $('.portfolio-filter .tag-all').click(function () {
-      selectedTag = null;
-      updateTagsAndCards();
     });
-
-    $('.portfolio-filter a:not(.tag-all)').click(function (ev) {
-      var tag = $(ev.target).text().toLowerCase();
-      selectedTag = selectedTag === tag ? null : tag;
-      updateTagsAndCards();
-    });
-
   },
 };
