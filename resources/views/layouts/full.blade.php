a182fc0d74154158406d61cd40374fa90676457e
Miro Mannino <miro.mannino@gmail.com>
2020-05-30 12:23:41 +0400

    full page template

---
+++ resources/views/layouts/full.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html {!! get_language_attributes() !!}>
+
+  @include('partials.head')
+
+  <body @php body_class() @endphp>
+    
+    @php do_action('get_header') @endphp
+    @include('partials.header')
+
+    <div class="header-img-container">
+        <!-- No image for this kind of page-->
+    </div>
+    
+    <div class="content-container content-container-full">
+      <div class="container fadeIn" role="document">
+        <div class="content row">
+          <main class="main fadeIn fadeInD1">
+            @yield('content')
+          </main>
+        </div>
+      </div>
+    </div>
+
+    @include('partials.footer-full')
+    @php do_action('get_footer') @endphp
+    @php wp_footer() @endphp
+    
+    @if (function_exists('get_field') && get_field('footer_script'))
+      {{!! the_field('footer_script') !!}}
+    @endif
+
+    @if (strlen(get_theme_mod('twitter_tag_code')) > 0)
+      {{!! get_theme_mod('twitter_tag_code') !!}}
+    @endif
+
+    @yield('additional-scripts')
+
+  </body>
+</html>
