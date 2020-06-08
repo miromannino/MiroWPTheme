59d381c586884d9317ba5dc9923f0fac5f8f98c5
Miro Mannino <miro.mannino@gmail.com>
2020-06-09 01:19:10 +0400

    home carousel

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,8 +1,20 @@
+import $ from 'jquery';
+
+// Home page scripts
 export default {
   init() {
-    // JavaScript to be fired on the home page
+    document.jq = $;
+    $(document).ready(function() {
+      var $carousel = $('.carousel.lazy');
+      $carousel.on('slide.bs.carousel', function() {
+        var $nextImg = $carousel.find('img[data-src]').eq(0);
+        $nextImg.attr('src', $nextImg.attr('data-src'));
+        $nextImg.removeAttr('data-src');
+      });
+    });
+
   },
   finalize() {
-    // JavaScript to be fired on the home page, after the init JS
+
   },
 };
