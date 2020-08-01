94f8b15856a744bda5234c2847d0d56aa8792342
Miro Mannino <miro.mannino@gmail.com>
2020-08-01 12:34:32 +0400

    show or hide page titles

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,4 +1,4 @@
-@php if (App::title() != '') { @endphp
+@php if (App::title() != '' && get_theme_mod('show_page_titles')) { @endphp
 <div class="page-header">
   <h1>{!! App::title() !!}</h1>
 </div>
